6de0b0df4a0ae69ec65695f0fc290bc92afe8bfe
Mandibull <mandibull@gmail.com>
2014-05-13 16:21:07 +0200

    Making install.sh a real sh script, not bash

--- install.sh
+++ install.sh
@@ -1,14 +1,10 @@
 #!/bin/sh
 
 deploy() {
-    local SRC=$1
-    local DST=$2
-
-    for i in `find $SRC -maxdepth 1 -type f` ; do
-        DSTFILE=$DST/`basename $i`
+    for i in `find $1 -maxdepth 1 -type f` ; do
+        DSTFILE="$2/`basename $i`"
         if [ -h "$DSTFILE" ] ; then
-            echo "nothing to do for $DSTFILE"
-            # FIXME: check if we use the right symlink
+            echo "nothing to do for $DSTFILE" # TODO: check if we use the right symlink
         elif [ -f "$DSTFILE" ] ; then
             echo "warning: $DSTFILE already exists, please get rid of it and restart this script"
         else
@@ -16,11 +12,10 @@ deploy() {
             ln -s "$i" "$DSTFILE"
         fi
     done
-
-    for i in `find $SRC -mindepth 1 -maxdepth 1 -type d` ; do
+    for i in `find $1 -mindepth 1 -maxdepth 1 -type d` ; do
         DIRNAME=`basename $i`
-        test -d $DST/$DIRNAME || mkdir $DST/$DIRNAME
-        deploy $SRC/$DIRNAME $DST/$DIRNAME
+        test -d $2/$DIRNAME || mkdir $2/$DIRNAME
+        deploy $1/$DIRNAME $2/$DIRNAME
     done
 }
 
