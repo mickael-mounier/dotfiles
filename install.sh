a1a9e0257941fd1717e9d049c6f162d2eed27ea2
Mandibull <mandibull@gmail.com>
2014-05-13 15:48:09 +0200

    The install.sh script is now recursive in subdirectories

--- install.sh
+++ install.sh
@@ -1,6 +1,29 @@
 #!/bin/sh
 
-# TODO: handle directories for .emacs.d & friends!
+deploy() {
+    local SRC=$1
+    local DST=$2
+
+    for i in `find $SRC -maxdepth 1 -type f` ; do
+        FILENAME=`basename $i`
+        if [ -h "$DST/$FILENAME" ] ; then
+            echo "nothing to do for $DST/$FILENAME"
+            # FIXME: check if we use the right symlink
+        elif [ -f "$DST/$FILENAME" ] ; then
+            echo "warning: $DST/$FILENAME already exists, please get rid of it and restart this script"
+        else
+            echo "linking $DST/$FILENAME"
+            ln -s "$i" "$DST/$FILENAME"
+            true
+        fi
+    done
+
+    for i in `find $SRC -mindepth 1 -maxdepth 1 -type d` ; do
+        DIRNAME=`basename $i`
+        test -d $DST/$DIRNAME || echo mkdir $DST/$DIRNAME
+        deploy $SRC/$DIRNAME $DST/$DIRNAME
+    done
+}
 
 if [ x"$(dirname $0)" = x"." ] ; then
     FILESDIR="$PWD/files"
@@ -8,15 +31,4 @@ else
     FILESDIR="$PWD/$(dirname $0)/files"
 fi
 
-for i in `find $FILESDIR -maxdepth 1 -type f` ; do
-    FILENAME=`basename $i`
-    if [ -h "$HOME/$FILENAME" ] ; then
-        echo "nothing to do for \"$FILENAME\""
-        # FIXME: maybe check if we use the right symlink?
-    elif [ -f "$HOME/$FILENAME" ] ; then
-        echo "WARNING: \"$FILENAME\" already exists, please get rid of it and restart this script"
-    else
-        echo "linking \"$FILENAME\""
-        ln -s "$i" "$HOME/$FILENAME"
-    fi
-done
+deploy $FILESDIR $HOME
