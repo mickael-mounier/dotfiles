95cc57fb874a646935b3580a29d62c05a6072b9d
Mandibull <mandibull@gmail.com>
2014-05-13 15:55:27 +0200

    Prettified install.sh

--- install.sh
+++ install.sh
@@ -5,23 +5,23 @@ deploy() {
     local DST=$2
 
     for i in `find $SRC -maxdepth 1 -type f` ; do
-        FILENAME=`basename $i`
-        if [ -h "$DST/$FILENAME" ] ; then
-            echo "nothing to do for $DST/$FILENAME"
+        DSTFILE=`basename $i`
+        if [ -h "$DSTFILE" ] ; then
+            echo "nothing to do for $DSTFILE"
             # FIXME: check if we use the right symlink
-        elif [ -f "$DST/$FILENAME" ] ; then
-            echo "warning: $DST/$FILENAME already exists, please get rid of it and restart this script"
+        elif [ -f "$DSTFILE" ] ; then
+            echo "warning: $DSTFILE already exists, please get rid of it and restart this script"
         else
-            echo "linking $DST/$FILENAME"
-            ln -s "$i" "$DST/$FILENAME"
+            echo "linking $DSTFILE"
+            ln -s "$i" "$DSTFILE"
             true
         fi
     done
 
     for i in `find $SRC -mindepth 1 -maxdepth 1 -type d` ; do
-        DIRNAME=`basename $i`
-        test -d $DST/$DIRNAME || echo mkdir $DST/$DIRNAME
-        deploy $SRC/$DIRNAME $DST/$DIRNAME
+        DSTDIR=`basename $i`
+        test -d $DSTDIR || echo mkdir $DSTDIR
+        deploy $SRC/$DIRNAME $DSTDIR
     done
 }
 
